62dd64b037b8a32d812d795710eee5974c6630f3
junseokNoh <ljs3271@naver.com>
2021-08-22 01:20:26 +0900

    #1 20925
    메이플스토리

--- codingTest/백준/20925 메이플스토리.py
+++ codingTest/백준/20925 메이플스토리.py
@@ -3,16 +3,16 @@ import sys
 N, T = map(int,sys.stdin.readline().split())
 records = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
 time_table = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
-dp = [[0 for _ in range(T+1)] for _ in range(N)]
+dp = [[0 if records[i][0] == 0 else -1 for _ in range(T+1)] for i in range(N)]
 
-for j in range(0, T+1):
-    for i, value in enumerate(records):
-        min_exp, exp = value[0], value[1]
-        dp[i][j+1] = dp[i][j] + exp
-        for k in range(0,N):
-            if i != k and j + time_table[i][k] <= T and dp[i][j] >= min_exp:
+for j in range(T):
+    for i in range(N):
+        if dp[i][j] == -1 : continue
+        min_exp, exp = records[i][0], records[i][1]
+        dp[i][j+1] = max(dp[i][j+1],dp[i][j] + exp)
+        for k in range(N):
+            if i != k and j + time_table[i][k] <= T and dp[i][j] >= records[k][0]:
                 dp[k][j+time_table[i][k]] = max(dp[k][j+time_table[i][k]], dp[i][j])
 
-print(max(map(max, dp)))
-
-
+print(dp)
+print(max(map(max,dp)))
