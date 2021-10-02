5748f111bb924b5655271a53182a218e27729df0
junseokNoh <ljs3271@naver.com>
2021-10-03 00:17:45 +0900

    Update 20056 마법사 사어와 파이어볼.py

--- codingTest/백준/20056 마법사 사어와 파이어볼.py
+++ codingTest/백준/20056 마법사 사어와 파이어볼.py
@@ -1,6 +1,4 @@
 import sys
-from collections import defaultdict
-import math
 
 N, M, K = map(int,sys.stdin.readline().split())
 arr = [[[] for _ in range(N+1)] for _ in range(N+1)]
@@ -40,42 +38,38 @@ def check(val):
         return False
     else:
         return True
-    # 이동 결과 row col을 내보내야함
-for i in range(K):
-    fire_dict = defaultdict(list)
+
+for _ in range(K):
+    arr = [[[] for _ in range(N+1)] for _ in range(N+1)]
     for fires_info in fires_infos:
         r,c,m,s,d = fires_info
         r,c = move(r,c,s,d)
-        fire_dict[str(r)+str(c)].append([r,c,m,s,d])
+        arr[r][c].append([r,c,m,s,d])
 
     new_fires_infos = []
-    for key in fire_dict.keys():
-        fire_cnt = len(fire_dict[key]); row = 0;col = 0;new_m = 0; new_s = 0; new_d = []
-        if fire_cnt == 1:
-            new_fires_infos.extend(fire_dict[key])
-            continue
-        for r,c,m,s,d in fire_dict[key]:
-            row = r
-            col = c
-            new_m += m
-            new_s += s
-            new_d.append(d)
+    for i in range(1,N+1):
+        for j in range(1,N+1):
+            fire_cnt = len(arr[i][j])
+            if fire_cnt == 1:
+                new_fires_infos.extend([arr[i][j]][0])
+            if fire_cnt >= 2:
+                new_m = 0; new_s = 0; new_d = []
+                for r,c,m,s,d in arr[i][j]:
+                    new_m += m
+                    new_s += s
+                    new_d.append(d)
 
-        new_m = new_m//5
-        new_s = new_s//fire_cnt
-        if check(new_d) : d_list = [0,2,4,6]
-        else: d_list = [1,3,5,7]
-
-        if new_m > 0:
-            for i in range(4):
-                new_fires_infos.append([row,col,new_m,new_s,d_list[i]])
+                new_m = new_m//5
+                new_s = new_s//fire_cnt
+                if check(new_d): d_list = [0,2,4,6]
+                else: d_list = [1,3,5,7]
+                if new_m > 0:
+                    for k in range(4):
+                        new_fires_infos.append([i,j,new_m,new_s,d_list[k]])
 
     fires_infos = new_fires_infos
 
 sumval = 0
-
-print(fires_infos)
 for info in fires_infos:
     sumval += info[2]
 print(sumval)
-
