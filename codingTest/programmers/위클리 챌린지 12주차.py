6d384a6fab8f76e44f7948593d1d0f39e9585d1c
junseokNoh <ljs3271@naver.com>
2021-11-01 23:53:26 +0900

    Update 위클리 챌린지 12주차.py

--- codingTest/programmers/위클리 챌린지 12주차.py
+++ codingTest/programmers/위클리 챌린지 12주차.py
@@ -1,3 +1,20 @@
+answer = -1
 def solution(k, dungeons):
-    answer = -1
+    size = len(dungeons)
+    visited = [True for _ in range(size)]
+
+    def DFS(depth, cnt, k, visited):
+        global answer
+        answer = max(answer, cnt)
+        for i in range(size):
+            if visited[i] and k >= dungeons[i][0]:
+                visited[i] = False
+                DFS(depth + 1, cnt + 1, k - dungeons[i][1], visited)
+                visited[i] = True
+
+    DFS(0, 0, k, visited)
     return answer
+
+# 일정 피로도를 사용해서 던전 탐험 가능
+# 최소 필요 피로도와, 소모 피로도가 있음
+#
